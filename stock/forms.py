c6f2752a1ecd3c9f31608e11af4bb93d7730eb04
alexanderkq <146338038+alexanderkq@users.noreply.github.com>
2023-09-29 23:29:09 +0300

    Init project

--- stock/forms.py
+++ stock/forms.py
@@ -4,8 +4,3 @@ from django import forms
 class BuySellForm(forms.Form):
     price = forms.DecimalField(widget=forms.NumberInput(attrs={'readonly': 'readonly'}))
     amount = forms.IntegerField()
-
-
-class BuySellForm1(forms.Form):
-    price = forms.DecimalField(widget=forms.NumberInput(attrs={'readonly': 'readonly'}))
-    amount = forms.IntegerField()
